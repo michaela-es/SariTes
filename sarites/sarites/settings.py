84afb3b795f0422b14862e0f2f8dfb5211a9c943
michaela-es <michaela.destrera@gmail.com>
2026-06-03 20:55:00 +0800

    add .env.example for supabase integration, update settings to support DATABASE_URL via dj-database-url, make tables more compact on mobile

--- sarites/sarites/settings.py
+++ sarites/sarites/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -55,10 +56,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sarites.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
