1c50b175db626ff45c168e9023b3245f95e5d943
michaela-es <michaela.destrera@gmail.com>
2026-06-03 21:30:06 +0800

    supabase connection established

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
-call .venv\Scripts\activate.bat
+cd /d "%~dp0"
+call sarites\.venv\Scripts\activate.bat
 echo Activated .venv — running: python manage.py runserver
+cd sarites
 python manage.py runserver
